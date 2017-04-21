3e03bf17b38e7eee829a07f62f017cefa13ff728
DennyChang <DennyChang@elytone.com.tw>
2017-04-21 16:58:22 +0800

    Feat: use buffers instead of mini to disable recentf

--- elisp/plug-helm.el
+++ elisp/plug-helm.el
@@ -10,7 +10,7 @@
   (setq-default helm-completion-in-region-fuzzy-match t)
   :config (helm-mode 1)
   (setq-default helm-boring-buffer-regexp-list (append helm-boring-buffer-regexp-list '("\\*")))
-  :bind (("C-p" . helm-mini)
+  :bind (("C-p" . helm-buffers-list)
 	 ("M-x" . helm-M-x)
 	 ("C-x C-f" . helm-find-files)
 	 ("C-f" . helm-occur)
