7d917c5dc42add1e5d5212a9d1004132024d08c5
DennyChang <DennyChang@elytone.com.tw>
2017-05-23 08:20:55 +0800

    Auto create custom file

--- init.el
+++ init.el
@@ -6,6 +6,8 @@
 
 ;;; Custom File
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(unless (file-exists-p custom-file)
+  (with-temp-buffer (write-file custom-file)))
 (load custom-file)
 
 ;; Basic Settings
